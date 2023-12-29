563f42f4038f024578c9fa0c0ab17dd59d981663
Kiyoung <rlaky45@naver.com>
2023-12-29 15:59:46 +0900

    feat: Initial Commit

---
+++ NVoiceMemoCloneCoding/NVoiceMemoCloneCoding/NVoiceMemoCloneCodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NVoiceMemoCloneCodingApp.swift
+//  NVoiceMemoCloneCoding
+//
+//  Created by 김기영 on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct NVoiceMemoCloneCodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
